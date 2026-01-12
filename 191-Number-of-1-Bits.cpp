67c83cfa5f4fea969522104c87588999250a67d3
gesinski <gesinskipawel@gmail.com>
2026-01-12 21:11:10 +0100

    binay 2

---
+++ 191-Number-of-1-Bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int counter = 0;
+
+        while (n) {
+            counter += n & 1;
+            n = n >> 1;
+        }
+
+        return counter;
+    }
+};
